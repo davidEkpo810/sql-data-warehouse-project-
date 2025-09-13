786ae88176a32dacfb76c2020f9c17ef6f45f95b
davidEkpo810 <uxdavid810@gmail.com>
2025-09-13 11:26:37 +0100

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,18 @@
+/*
+===========================================================
+Creating Database
+===========================================================
+Script purpose:
+    This script creates a new database named 'Datawarehouse' after cchecking if it already exist.
+    If database exists, it is dropped and recreated.
+
+WARNING:
+    Running this script will drop the entire 'Datawarehouse' database if it exists.
+    All data in database will be permanently deleted. Proceed with caution
+    and ensure you have proper backups before running this script.
+*/
+
+-- Drop and recreate the 'Datawarehouse' Database if it already exists
+DROP DATABASE IF EXISTS Datawarehouse;
+CREATE DATABASE Datawarehouse CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
